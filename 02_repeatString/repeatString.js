b7667d45940da973870295c495a0565d2c5f8cf1
Yannic1992 <yannic1992@gmx.net>
2022-09-13 23:10:11 +0200

    Improve solution for exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -2,19 +2,12 @@ const repeatString = function(string, num) {
     if(string == ""){
         return "";
     }else{
-        if(num < 0){
-            return "ERROR";
-        }else if(num == 0){
-            return "";
-        }else if(num == 1){
-            return string;
-        }else{
-            let newString = "";
-            for(let i = 1; i <= num; i++){
-                newString = newString + string;
-            }
-            return newString;
+        if(num < 0) return "ERROR";
+        let newString = "";
+        for(let i = 1; i <= num; i++){
+            newString += string;
         }
+        return newString;
     }
 };
 
