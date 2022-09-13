353d2a071295a3a414199d928c17087337cda68b
Yannic1992 <yannic1992@gmx.net>
2022-09-13 22:56:52 +0200

    Complete exercise 02 of javascript-exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,21 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if(string == ""){
+        return "";
+    }else{
+        if(num < 0){
+            return "ERROR";
+        }else if(num == 0){
+            return "";
+        }else if(num == 1){
+            return string;
+        }else{
+            let newString = "";
+            for(let i = 1; i <= num; i++){
+                newString = newString + string;
+            }
+            return newString;
+        }
+    }
 };
 
 // Do not edit below this line
