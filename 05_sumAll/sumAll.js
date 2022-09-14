15aa8bd5475ebb49486b142a6b2543a64fce1773
Yannic1992 <yannic1992@gmx.net>
2022-09-14 12:23:38 +0200

    Complete exercise 05 of javascript-exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(...input) {
+    if(input.every(ele => typeof ele === "number") && 
+       input.every(value => value > 0)){
+        if(input[0] > input[1]) input.sort();
+        return ((input[1]-input[0])+1)*(input[0]+input[1])/2; 
+    }else return "ERROR";
 };
 
 // Do not edit below this line
