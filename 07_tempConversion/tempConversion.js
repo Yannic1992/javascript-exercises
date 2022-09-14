1e8f47181353c15dcc9f56f5cd679970ff8743ef
Yannic1992 <yannic1992@gmx.net>
2022-09-14 12:43:39 +0200

    Complete exercise 07 of javascript-exercises

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(tempf) {
+  return Math.round(((tempf-32)/1.8)*10)/10;
 };
 
-const ctof = function() {
-
+const ctof = function(tempc) {
+  return Math.round((tempc*1.8+32)*10)/10;
 };
 
 // Do not edit below this line
