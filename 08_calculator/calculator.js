a1208cf045af4b2b47816e36e67490e0367c272e
Yannic1992 <yannic1992@gmx.net>
2023-01-28 11:57:57 +0100

    Complete exercise 08 of javascript-exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(input) {
+	let output = 0;
+  input.forEach(element => {
+    output += element;
+  });
+  return output;
 };
 
-const multiply = function() {
-
+const multiply = function(input) {
+  let output = 1;
+  input.forEach(element => {
+    output *= element;
+  });
+  return output;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	return (x > 1) ? x * factorial(x-1) : 1;
 };
 
 // Do not edit below this line
