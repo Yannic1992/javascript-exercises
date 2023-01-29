969fa5bfda1424539445fc0e4eecc091b71d2aaa
Yannic1992 <yannic1992@gmx.net>
2023-01-29 14:10:37 +0100

    Complete exercise 09 of javascript-exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,15 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    let regExPattern = /[^A-Za-z]/g;
+    let inputRegEx = input.replace(regExPattern, "");
+    let inputLowerCase = inputRegEx.toLowerCase();
+    let arrayInput = [...inputLowerCase];
+    let arrayReversed = arrayInput.reverse();
+    let stringReversed = arrayReversed.join("");
+    if(inputLowerCase === stringReversed) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
