29b78a18cd6d102001ae8b17f2153ad7af574570
Yannic1992 <yannic1992@gmx.net>
2023-01-29 22:46:46 +0100

    Complete exercise 10 of javascript-exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+    if(input < 0){
+        return "OOPS"
+    } else {
+        let a = 0;
+        let b = 1;
+        let c;
+        for(let i = 0; i < input; i++) {
+            c = b;
+            b = b + a;
+            a = c;
+        }
+        return a;
+    }
 };
 
 // Do not edit below this line
