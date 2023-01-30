a91b28cfeb76c69e05ad89aab20e688b88579170
Yannic1992 <yannic1992@gmx.net>
2023-01-30 11:37:58 +0100

    Complete exercise 12 of javascript-exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const getAge = function(birthYear, deathYear) {
+    if(!deathYear) deathYear = new Date().getFullYear();
+    return deathYear - birthYear;
+};
 
+const findTheOldest = function(input) {
+    return input.reduce((currentOldest, nextPerson) => {
+        let currentAge = getAge(currentOldest.yearOfBirth, currentOldest.yearOfDeath);
+        let nextAge = getAge(nextPerson.yearOfBirth, nextPerson.yearOfDeath);
+        return (currentAge < nextAge) ? nextPerson : currentOldest;
+    })
 };
 
 // Do not edit below this line
